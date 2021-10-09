3bc8d707fae18d6bdf487bfee0d4a7455e276514
= <ayushsingla909@gmail.com>
2021-10-10 00:45:56 +0530

    Product Detailed Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,26 +1,30 @@
 import React from "react"
 import { Card } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const Product = ({ product }) => {
   return (
-    <div>
-      <Card className='my-3 p-3 rounded bg-light'>
-        <a href={`/product/${product._id}`}>
-          <Card.Img src={product.image}></Card.Img>
-        </a>
-        <Card.Body className='my-3 p-3 rounded'>
-          <a href={`/product/${product._id}`}>
-            <Card.Title as='div'></Card.Title>
-          </a>
-          <Card.Text as='div'>
-            <div className='my-3'>
-              {product.rating} as {product.numReviews}
-            </div>
-          </Card.Text>
-          <Card.Text as='h3'>${product.price}</Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+    <Card className='my-3 p-3 rounded bg-light'>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
 
