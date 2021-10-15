716ef2e2f5171f75a9c15d47c0df5b250ba01ef1
= <ayushsingla909@gmail.com>
2021-10-16 02:06:43 +0530

    Getting Started with MongoDb - Error Handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import products from "./data/products.js"
 import connectDB from "./config/db.js"
 import colors from "colors"
+import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 connectDB()
@@ -15,6 +17,10 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
+
+app.use("/api/products", productRoutes)
+app.use(notFound)
+app.use(errorHandler)
 app.get("/", (req, res) => {
   res.send("API is running.......!!!!!!!!!!")
 })
