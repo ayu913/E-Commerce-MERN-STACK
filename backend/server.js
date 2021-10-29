1c8003789ee8bc84c8bb58edb8343dec0204002c
= <ayushsingla909@gmail.com>
2021-10-30 02:42:38 +0530

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,13 @@ import products from "./data/products.js"
 import connectDB from "./config/db.js"
 import colors from "colors"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 connectDB()
 const app = express()
+app.use(express.json())
 const PORT = process.env.PORT || 5000
 
 app.listen(
@@ -17,7 +19,7 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
-
+app.use("/api/users", userRoutes)
 app.use("/api/products", productRoutes)
 app.use(notFound)
 app.use(errorHandler)
