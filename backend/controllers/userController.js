a2d67147d61921f6b310d62a4fc64f37d4e03819
= <ayushsingla909@gmail.com>
2021-11-16 21:17:11 +0530

    Admin User Delete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,18 +61,6 @@ const registerUser = asyncHandler(async (req, res) => {
     email,
     password,
   })
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //     token: generateToken(user._id),
-  //   })
-  // } else {
-  //   res.status(401)
-  //   throw new Error("Invalid email or password")
-  // }
 
   if (user) {
     res.status(201).json({
@@ -80,6 +68,7 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -123,4 +112,26 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    await user.remove()
+    res.json({ message: "User removed" })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   getUsers,
   registerUser,
   updateUserProfile,
+  deleteUser,
 } from "../controllers/userController.js"
 import { admin, protect } from "../middleware/authMiddleware.js"
 
@@ -15,5 +16,6 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
 router.route("/").post(registerUser).get(protect, admin, getUsers)
+router.route("/:id").delete(protect, admin, deleteUser)
 
 export default router
