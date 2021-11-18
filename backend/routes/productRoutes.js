62d21f95d9f8e1abfe7930f91189ae9b03e20395
= <ayushsingla909@gmail.com>
2021-11-18 22:35:53 +0530

    Admin Delete Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router()
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js"
+
+import { protect, admin } from "../middleware/authMiddleware.js"
 
 // // @desc  Fetch all products
 // // @route  GET/api/products
@@ -34,5 +37,5 @@ import {
 //   })
 // )
 router.route("/").get(getProducts)
-router.route("/:id").get(getProductById)
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct)
 export default router
