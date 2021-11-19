e638e2ad444c513a5386fa74e6e38743e4ec8ba3
= <ayushsingla909@gmail.com>
2021-11-19 20:15:04 +0530

    Create & Update Product Endpoints

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,36 +6,16 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js"
 
 import { protect, admin } from "../middleware/authMiddleware.js"
 
-// // @desc  Fetch all products
-// // @route  GET/api/products
-// // acces   Public
-// router.get(
-//   "/",
-//   asyncHandler(async (req, res) => {
-//     const products = await Product.find({})
-//     res.json(products)
-//   })
-// )
-// // @desc    Fetch single product
-// // @route   GET /api/products/:id
-// // @access  Public
-// router.get(
-//   "/:id",
-//   asyncHandler(async (req, res) => {
-//     const product = await Product.findById(req.params.id)
-
-//     if (product) {
-//       res.json(product)
-//     } else {
-//       res.status(404)
-//       throw new Error("Product not found")
-//     }
-//   })
-// )
-router.route("/").get(getProducts)
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct)
+router.route("/").get(getProducts).post(protect, admin, createProduct)
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 export default router
