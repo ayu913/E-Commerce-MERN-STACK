24f9e27e017f8f00931cd6233c65815af9b0d038
= <ayushsingla909@gmail.com>
2021-11-20 19:14:54 +0530

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
+import path from "path"
 
 dotenv.config()
 connectDB()
@@ -23,9 +25,14 @@ app.listen(
 app.use("/api/users", userRoutes)
 app.use("/api/products", productRoutes)
 app.use("/api/orders", orderRoutes)
+app.use("/api/upload", uploadRoutes)
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
 app.use(notFound)
 app.use(errorHandler)
 app.get("/", (req, res) => {
