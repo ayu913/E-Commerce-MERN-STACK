5f85defda52c9430b75296814426712344db6c7e
= <ayushsingla909@gmail.com>
2021-11-25 21:29:09 +0530

    Deploy Heroku

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,21 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
 app.use(notFound)
 app.use(errorHandler)
-app.get("/", (req, res) => {
-  res.send("API is running.......!!!!!!!!!!")
-})
+// app.get("/", (req, res) => {
+//   res.send("API is running.......!!!!!!!!!!")
+// })
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
 
 // app.get("/api/products", (req, res) => {
 //   res.json(products)
