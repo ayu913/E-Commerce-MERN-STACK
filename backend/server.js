7c287c3e5612a9da1d77c4d556ffd0d34052c38b
= <ayushsingla909@gmail.com>
2021-11-25 22:26:13 +0530

    Production error Solved

--- backend/server.js
+++ backend/server.js
@@ -10,39 +10,29 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 import path from "path"
 import morgan from "morgan"
-
 dotenv.config()
+
 connectDB()
+
 const app = express()
-app.use(express.json())
-const PORT = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-)
-app.use("/api/users", userRoutes)
+app.use(express.json())
+
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/upload", uploadRoutes)
+
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
-
-app.use(notFound)
-app.use(errorHandler)
-// app.get("/", (req, res) => {
-//   res.send("API is running.......!!!!!!!!!!")
-// })
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")))
@@ -56,6 +46,17 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
 // app.get("/api/products", (req, res) => {
 //   res.json(products)
 // })
